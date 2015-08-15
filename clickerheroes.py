c33f11944161087649b2407de42d8e5f54c8bb96
sutherlandpb <sutherlandpb@gmail.com>
2015-08-15 18:11:59 -0500

    Initial check-in

---
+++ clickerheroes.py
@@ -0,0 +1,14 @@
+wait(1)
+
+Settings.minSimilarity = .08
+while 1==1: 
+    
+    if exists("1439348096453.png"):
+        click("1439348096453.png")
+    if exists("1439384387472.png"): 
+        doubleClick(Pattern("1439384387472.png").targetOffset(-152,9))
+    if exists("1439365122487.png"): 
+        hover("1439365122487.png")
+        type("689")
+        
+        
